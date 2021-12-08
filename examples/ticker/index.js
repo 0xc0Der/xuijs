14dd32cf57bc94a5f0c575aacbe4e7a616444127
0xc0Der <59133155+0xc0Der@users.noreply.github.com>
2021-12-08 12:40:25 +0000

    renamed countup to ticker and improved it

---
+++ examples/ticker/index.js
@@ -0,0 +1,38 @@
+import { Xui, XuiElement, Variable } from '../../src/index.js';
+
+export class Ticker extends XuiElement {
+
+    constructor(el) {
+        super(el);
+
+        this.ticks = new Variable(0);
+        this.state = new Variable(false);
+    }
+
+    getTicks(ticks) {
+        return `${ticks}`;
+    }
+
+    resetTicks() {
+        this.ticks.value = 0;
+    }
+
+    getState(state) {
+        return state ? 'stop' : 'start';
+    }
+
+    switchState() {
+        if(this.state.value) {
+            window.clearInterval(this.ticker);
+        } else {
+            this.ticker = window.setInterval(() => this.ticks.value++, 1000);
+        }
+
+        this.state.value = !this.state.value;
+    }
+
+}
+
+const App = new Xui();
+
+App.init(document.body);
